9b75fa4993665d78e0125aa687c8581c919b236d
wenchen123546 <122010367+wenchen123546@users.noreply.github.com>
2025-11-08 23:35:09 +0800

    Update constants.js

--- socket/constants.js
+++ socket/constants.js
@@ -1,5 +1,4 @@
 // /socket/constants.js
-// (我們將 Keys 移到這裡，以便路由和 Socket 處理器都能共用)
 
 const KEY_CURRENT_NUMBER = 'callsys:number';
 const KEY_PASSED_NUMBERS = 'callsys:passed';
@@ -10,6 +9,7 @@ const KEY_IS_PUBLIC = 'callsys:isPublic';
 const KEY_ADMIN_LAYOUT = 'callsys:admin-layout'; 
 const KEY_ADMIN_LOG = 'callsys:admin-log'; 
 const KEY_USERS_HASH = 'callsys:users'; 
+const KEY_ONLINE_ADMINS = 'callsys:online:admins'; // 【v3.8】 新增
 
 module.exports = {
     KEY_CURRENT_NUMBER,
@@ -20,5 +20,6 @@ module.exports = {
     KEY_IS_PUBLIC,
     KEY_ADMIN_LAYOUT,
     KEY_ADMIN_LOG,
-    KEY_USERS_HASH
+    KEY_USERS_HASH,
+    KEY_ONLINE_ADMINS // 【v3.8】 新增
 };
